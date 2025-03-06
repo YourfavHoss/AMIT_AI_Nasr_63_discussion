183f7e3a36e40198980d38ef03321374b4d8dd11
YourfavHoss <mohamedhoss1000@outlook.com>
2025-03-06 14:45:51 +0200

    modified comments

--- Task_no_1.py
+++ Task_no_1.py
@@ -1,7 +1,7 @@
 
 """
 Lessons revised:1- isinstance(input,type(s)) 
-                2- trunary operator 
+                2- ternary operator 
                 3- .find() method for class str
                 4- squared = map(lambda x: x ** 2, numbers) 
                 5- isinstance bool method print(isinstance(1.0,(float,str)))
@@ -15,6 +15,7 @@ Lessons revised:1- isinstance(input,type(s))
 
 input_mail = input('Please enter your E-mail') # string by default
 
+### method for mail validation ###
 
 def check_valid_mail(string_mail:str) -> bool:
     count_at_sign = string_mail.count('@')
@@ -25,8 +26,13 @@ def check_valid_mail(string_mail:str) -> bool:
     
     return flag_1 and flag_2
 
-print(check_valid_mail(input_mail))
+#print(check_valid_mail(input_mail))
 
+
+### method for username extraction ###
+
+def extract_username(input_mail: str, mail_valid_status: bool) -> bool:
+    pass
 
  
 
